aeeeafe3a6e8fc8ba446b59ec42f4e0b5fe7a9fe
miedor <50088042+miedor@users.noreply.github.com>
2026-06-25 18:01:56 -0600

    Create ImportValoresActivoRequest.php

---
+++ app/Http/Requests/ImportValoresActivoRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ImportValoresActivoRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'archivo_csv' => [
+                'required',
+                'file',
+                'mimes:csv,txt',
+                'max:5120',
+            ],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'archivo_csv.required' => 'Debes seleccionar un archivo CSV para importar.',
+            'archivo_csv.file' => 'El archivo seleccionado no es válido.',
+            'archivo_csv.mimes' => 'El archivo debe tener extensión CSV o TXT.',
+            'archivo_csv.max' => 'El archivo no debe superar los 5 MB.',
+        ];
+    }
+}
