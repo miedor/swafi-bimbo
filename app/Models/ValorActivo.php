3c1063b1e92a361a8d2d9d1ce730f8fd67a884f5
miedor <50088042+miedor@users.noreply.github.com>
2026-07-12 16:48:44 -0600

    Update ValorActivo.php

--- app/Models/ValorActivo.php
+++ app/Models/ValorActivo.php
@@ -12,10 +12,18 @@ class ValorActivo extends Model
         'numero_activo',
         'valor_fiscal',
         'valor_financiero',
+        'moneda',
+        'tipo_cambio',
+        'fecha_tipo_cambio',
+        'origen_tipo_cambio',
         'depreciacion_acumulada',
         'valor_en_libros',
         'vida_util_meses',
         'estatus_contable',
+        'motivo_cambio',
+        'cfdi_validacion_id',
+        'conciliacion_cfdi',
+        'conciliacion_detalle',
         'fecha_corte',
         'registrado_por',
     ];
@@ -23,13 +31,21 @@ class ValorActivo extends Model
     protected $casts = [
         'valor_fiscal' => 'decimal:2',
         'valor_financiero' => 'decimal:2',
+        'tipo_cambio' => 'decimal:6',
+        'fecha_tipo_cambio' => 'date',
         'depreciacion_acumulada' => 'decimal:2',
         'valor_en_libros' => 'decimal:2',
         'fecha_corte' => 'date',
+        'conciliacion_detalle' => 'array',
     ];
 
     public function activo()
     {
         return $this->belongsTo(Activo::class, 'numero_activo', 'numero_activo');
+    }
+
+    public function cfdiValidacion()
+    {
+        return $this->belongsTo(CfdiValidacion::class, 'cfdi_validacion_id');
     }
 }
